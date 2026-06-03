daaa9aa0c0d752632e773652b700d5b2b9c7000b
David Quan <david.quan@broadcom.com>
2026-06-03 15:17:41 +0800

    remove unused  and  in pkg/dashboards/etcd/etcd.go and pkg/dashbaords/kubernetes/compute_resources/cluster.go, because switch to
    Signed-off-by: David Quan <david.quan@broadcom.com>

--- pkg/dashboards/kubernetes/compute_resources/cluster.go
+++ pkg/dashboards/kubernetes/compute_resources/cluster.go
@@ -19,9 +19,6 @@ import (
 	"github.com/perses/community-mixins/pkg/promql"
 	"github.com/perses/perses/go-sdk/dashboard"
 	panelgroup "github.com/perses/perses/go-sdk/panel-group"
-
-	listVar "github.com/perses/perses/go-sdk/variable/list-variable"
-	labelValuesVar "github.com/perses/plugins/prometheus/sdk/go/variable/label-values"
 )
 
 func withClusterStatsGroup(datasource string, labelMatcher promql.LabelMatcher) dashboard.Option {
